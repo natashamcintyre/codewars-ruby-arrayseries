ae89631aaea0847ad313390637877d49a61b85d0
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-15 21:17:08 +0000

    code working

--- lib/arrayseries1.rb
+++ lib/arrayseries1.rb
@@ -1,3 +1,9 @@
 def min_sum(arr)
-  22
+  sorted_arr = arr.sort
+  total = 0
+  for i in 0...(arr.size / 2)
+    number_sum = sorted_arr[i] * sorted_arr[arr.size - 1 - i]
+    total += number_sum
+  end
+  total
 end
--- spec/arrayseries1_spec.rb
+++ spec/arrayseries1_spec.rb
@@ -4,4 +4,7 @@ describe 'min_sum' do
   it 'returns 22 when passed [5,4,2,3]' do
     expect(min_sum([5,4,2,3])).to eq 22
   end
+  it 'returns 342 when passed [12,6,10,26,3,24]' do
+    expect(min_sum([12,6,10,26,3,24])).to eq 342
+  end
 end
