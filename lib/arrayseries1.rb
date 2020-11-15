0e20ecc07b4435f3ffd6e6d7b880e5dde38b4e1d
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-15 21:29:42 +0000

    final refactoring and reflections from codewars learning

--- lib/arrayseries1.rb
+++ lib/arrayseries1.rb
@@ -1,9 +1,14 @@
 def min_sum(arr)
-  sorted_arr = arr.sort
-  total = 0
-  for i in 0...(arr.size / 2)
-    number_sum = sorted_arr[i] * sorted_arr[arr.size - 1 - i]
-    total += number_sum
+  total = 0 # initialize variable for total at 0
+  for i in 0...(arr.size / 2) # work through each index value until half way
+    total += arr.sort[i] * arr.sort[arr.size - 1 - i]
   end
   total
 end
+
+# best codewars solution uses map
+# def min_sum(arr)
+#   arr.sort! SORTS IN PLACE SO THAT YOU CAN CONTINUE CALLING IT ARR
+    # for each number from 0 to half arr size, multiply first by last
+#   (0...arr.size/2).map { |i| arr[i] * arr[-i-1] } .sum
+# end
